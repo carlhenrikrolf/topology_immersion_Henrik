0190d3ef0d6e0b32134f7647779b71da909b3a1b
CarlHenrikRolf <henrikaslund@tsf-428-wpa-4-065.epfl.ch>
2017-11-10 16:55:54 +0100

    Wiggling fixes and diffusion

--- exercises/refactored_ml_on_1_to_3/cifar/confusion_matrix.m
+++ exercises/refactored_ml_on_1_to_3/cifar/confusion_matrix.m
@@ -1,7 +1,9 @@
+% Load parameters
 params = load('../params.mat');
 code_to_data = ['../', params.paths.code_to_data, 'pixelations/cifar/'];
 n_shapes = params.shapes.n_shapes;
 
+% Find the latest epoch
 flag = true;
 last_epoch = 1;
 while flag
@@ -13,22 +15,28 @@ while flag
     end
 end
 
+% Load net from the latest epoch
 temp = load([code_to_data, 'nn_export/net-epoch-',...
              num2str(last_epoch), '.mat']);
 net = temp.net;
 net = vl_simplenn_tidy(net);
 net.layers{end}.type = 'softmax';
+
+% Load image database
 imdb = load([code_to_data, 'imdb.mat']);
 
+% Construct confusion matrix
 val_set = find(imdb.images.set == 2);
 output = zeros(n_shapes,n_shapes);
 for val_sample = val_set
     im = imdb.images.data(:,:,:,val_sample);
     res = vl_simplenn(net,im);
-    scores = squeeze(gather(res(end).x));
+    scores = squeeze(gather(res(end).x)); % Lots of weird stuff, see MatConvNet's webpage
     [~, prediction] = max(scores);
     label = imdb.images.labels(val_sample);
     output(prediction,label) = output(prediction,label) + 1;
 end
+
+% output
 disp('prediction x label confusion matrix:')
 disp(output)
